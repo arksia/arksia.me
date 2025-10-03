74e0a4e50df42292ac400f15aa416a8ed2154573
arkwq <354959692@qq.com>
2025-10-03 10:58:45 +0800

    feat: enhance page transition with animation

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,30 @@
 <template>
   <NavBar />
   <main class="main">
-    <RouterView />
+    <Transition name="fade">
+      <RouterView />
+    </Transition>
   </main>
 </template>
 
 <style scoped>
 .main {
   padding: 2.5rem 1rem;
+}
+
+.fade-enter-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-leave-active {
+  transition: opacity 0.4s ease-out;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
